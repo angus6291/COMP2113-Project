fa760d9c963f4d777c03e67ae3284c3f7cad29f3
angus6291 <93399469+angus6291@users.noreply.github.com>
2021-11-29 19:16:30 +0800

    input comment for kicking start the bingo_page

--- output.cpp
+++ output.cpp
@@ -1,6 +1,11 @@
+//for output and testing whether there's bingo
+//vector will be set for dynamic storage
 #include <iostream>
+#include " "
 using namespace std;
 
 void output() {
-  
+  int a;
+  int b;
+
 }

afa9764645a6f3286d36200f65114e1e3e08dffb
angus6291 <93399469+angus6291@users.noreply.github.com>
2021-11-30 16:02:58 +0800

    update from output to merga run_game tgt

--- run_game.cpp
+++ run_game.cpp
@@ -1,4 +1,4 @@
-//for output and testing whether there's bingo
+//for running the game and output status per every guess
 //first of all seperate into either 3*3 or 4*4
 //vector will be set for dynamic storage
 #include <iostream>
@@ -9,6 +9,7 @@ using namespace std;
 
 void output(int Number_of_terms_of_vector_v) {
   // if 3*3 grid, create {1,2,3,...,7,8,9}
+  vector<str>output;
   if (Number_of_terms_of_vector_v == 3) {
     //declare output vector for storing outputs when grid = 3*3
     vector<str>output;
@@ -20,9 +21,10 @@ void output(int Number_of_terms_of_vector_v) {
   // if 4*4 grid, create {1,2,3,...,14,15,16}
   else if (Number_of_terms_of_vector_v == 4) {
     //declare output vector for storing outputs when grid = 4*4
-    vector<str>output;
     for (int i=0; i<4^2, i++) {
       string s = to_string (i+1);
       output[i] = s;
     }
   }
+
+}

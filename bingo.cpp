67c87841281f5dd7fe6eb39423a97f3912c13fd4
angus6291 <93399469+angus6291@users.noreply.github.com>
2021-11-30 14:44:06 +0800

    change 1445 30/11

--- bingo.cpp
+++ bingo.cpp
@@ -8,16 +8,21 @@
 using namespace std;
 
 void output(int Number_of_terms_of_vector_v) {
-  // if 3*3 grid, create {1,2,3,..., 7,8,9}
+  // if 3*3 grid, create {1,2,3,...,7,8,9}
   if (Number_of_terms_of_vector_v == 3) {
-    vector<str>
-
+    //declare output vector for storing outputs when grid = 3*3
+    vector<str>output;
+    for (int i=0; i<3^2, i++) {
+      string s = to_string (i+1);
+      output[i] = s;
+    }
   }
-
-
-
-
-  int a;
-  int b;
-
-}
+  // if 4*4 grid, create {1,2,3,...,14,15,16}
+  else if (Number_of_terms_of_vector_v == 4) {
+    //declare output vector for storing outputs when grid = 4*4
+    vector<str>output;
+    for (int i=0; i<4^2, i++) {
+      string s = to_string (i+1);
+      output[i] = s;
+    }
+  }

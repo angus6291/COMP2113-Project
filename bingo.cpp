850f4b246a1050f23e0dce3a6e33a6d34cdeac81
angus6291 <93399469+angus6291@users.noreply.github.com>
2021-11-30 11:35:47 +0800

    chan 1135 30/11

--- bingo.cpp
+++ bingo.cpp
@@ -2,6 +2,7 @@
 //first of all seperate into either 3*3 or 4*4
 //vector will be set for dynamic storage
 #include <iostream>
+#include <string>
 #include <vector>
 #include " "
 using namespace std;
@@ -9,6 +10,7 @@ using namespace std;
 void output(int Number_of_terms_of_vector_v) {
   // if 3*3 grid, create {1,2,3,..., 7,8,9}
   if (Number_of_terms_of_vector_v == 3) {
+    vector<str>
 
   }
 

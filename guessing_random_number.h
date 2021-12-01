5401d47910628c6a3e3c837d80a675cf5ead0a04
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-01 21:14:18 +0800

    Update and rename random_guessing_number.h to guessing_random_number.h

---
+++ guessing_random_number.h
@@ -0,0 +1,6 @@
+#ifndef GUESSING_RANDOM_NUMBER_H
+#define GUESSING_RANDOM_NUMBER_H
+
+void rungame();
+
+#endif
--- random_guessing_number.h
+++
@@ -1,6 +0,0 @@
-#ifndef RANDOM_GUESSING_NUMBER_H
-#define RANDOM_GUESSING_NUMBER_H
-
-void rungame();
-
-#endif

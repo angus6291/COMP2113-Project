c72f81495e725c1174753778def99c43b62c4a5a
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-01 19:48:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,39 +6,46 @@
 using namespace std;
 
 int main() {
-  cout << "Welcome to the 'Guess the number game'!!!" <<endl; //Welcoming
-
-  while (True) {
+  cout << "Welcome to this game! Try your luck and see if you are lucky or not today!!!" <<endl; //Welcoming
+  
     // instruction code
     char instruction_code;
-    cout << "Please enter the instruction code: 'L', Previous saved game will be loaded; ";
-    cout << "'N', New game will be loaded; 'Y' Leave game";
+    cout << "Enter 'L' to load the previous saved game" << endl << "Enter 'S' to start a new game" << endl;
+    cout << "or enter 'Q' to quit the game" << endl;
     cin >> instruction_code;
+    while (instruction_code != 'Q') {
+      if (instruction_code != 'L' && instruction_code != 'S') {
+        cout << "Invalid input" << endl << "Please enter 'L', 'S' or 'Q' to continue" << endl;
+        cin >> instruction_code;
+      }
     if (instruction_code == 'L') {
       //to be implemented load from save
     }
-    else if (instruction_code == 'N') {
+    if (instruction_code == 'S') {
       //run new game
       int Number_of_terms_of_vector_v; //Decide either 3*3 or 4*4 grid
       int Number_of_attempts_allowed; //universal number of attempt allowed
-      cout << "Please Input the number of grid you want to play (3/4): " >> endl;
+      cout << "Input '3' to have a 3x3 bingo board or '4' to have a 4x4 bingo board" << endl;
       cin >> Number_of_terms_of_vector_v;
       if (Number_of_terms_of_vector_v == 3) {
         Number_of_attempts_allowed = 7; //declare max number of attempt if input grid is 3
       }
-      else if (Number_of_terms_of_vector_v == 4) {
+      if (Number_of_terms_of_vector_v == 4) {
         Number_of_attempts_allowed = 12; //declare max number of attempt if input grid is 3
       }
-      run_game(Number_of_terms_of_vector_v, Number_of_attempts_allowed) //run the core game
-      break
+      if (Number_of_terms_of_vector_v != 3 && Number_of_terms_of_vector_v != 4) {
+        cout << "Sorry, please input either '3' or '4' only. Thank you. " << endl;
+        cin >> Number_of_terms_of_vector_v;
+      }
+      run_game(Number_of_terms_of_vector_v, Number_of_attempts_allowed); //run the 'guessing random number' game
+      //load bingo board
+      
     }
-    else if (instruction_code == 'Y') {
+    }
+    if (instruction_code == 'Q') {
       //Leave game
-      cout << "Thank you, see you next time!!"
-    }
-    else if (instruction_code != 'L' || instruction_code != 'N' || instruction_code != 'Y') {
-      //re-enter code
-      cout << "Invaid input Please re-enter instruction code"
+      cout << "Thank you! \nSee you next time!!";
+      exit(1);
     }
 
     cout << "This is the end of the game, see u!!" << endl;

7e6a272384c141db35340fee338305fc9fa094ec
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-02 04:05:28 +0800

    Update guessing_random_number.h

--- guessing_random_number.h
+++ guessing_random_number.h
@@ -1,6 +1,6 @@
 #ifndef GUESSING_RANDOM_NUMBER_H
 #define GUESSING_RANDOM_NUMBER_H
 
-void rungame();
+void run_guessing_random_number_game(remaining_numbers_not_guessed, Number_of_attempts_allowed, Number_of_terms_of_vector_v);
 
 #endif

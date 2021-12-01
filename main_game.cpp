b80942c1c9af1a669db07332d823debd6192173a
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-02 02:54:12 +0800

    Update main_game.cpp

--- main_game.cpp
+++ main_game.cpp
@@ -2,10 +2,19 @@
 #include <iostream>
 #include <vector>
 #include <fstream> // for file input and output ('saved game')
-#include "random_guessing_number.h"
+#include <cstdlib> // for srand(), rand()
+#include "guessing_random_number.h"
 using namespace std;
 
-vector<int> processed(vector<int>& remaining_numbers_not_guessed, vector<int>& numbers_already_guessed) {
+vector<int>remaining_numbers_not_guessed;
+
+vector<int>numbers_already_guessed;
+
+vector<int>numbers_guessed_right;
+
+int Number_of_attempts_allowed;
+
+vector<int> processed(vector<int> remaining_numbers_not_guessed, vector<int> numbers_already_guessed) {
   // delete items in the vector 'numbers_already_guessed' from the vector 'remaining_numbers_not_guessed'
   int number_of_numbers_already_guessed = numbers_already_guessed.size();
   if (number_of_numbers_already_guessed == 0) {
@@ -33,12 +42,96 @@ vector<int> processed(vector<int>& remaining_numbers_not_guessed, vector<int>& n
   return remaining_numbers_not_guessed;
 }
 
+void run_guessing_random_number_game(vector<int> remaining_numbers_not_guessed, int Number_of_attempts_allowed, int Number_of_terms_of_vector_v) {
+  int X=remaining_numbers_not_guessed.size();
+  if (X == 0) {
+    break;
+  }
+  srand(time(NULL)); // initialize the seed for rand()
+  // the index must be smaller than remaining_numbers_not_guessed.size(), can start from 0
+  int index_of_number_to_be_guessed_in_the_remaining_numbers_list = rand() % X;
+  int number_to_be_guessed = remaining_numbers_not_guessed[index_of_number_to_be_guessed_in_the_remaining_numbers_list];
+  bool is_guessed_correctly = false;
+  int remaining_number_of_attempts_allowed = Number_of_attempts_allowed;
+  int guess;
+  while (remaining_number_of_attempts_allowed > 0) {
+    if (Number_of_terms_of_vector_v == 3) {
+      cout << "Please make a guess (1 - 9). ";
+      if (remaining_number_of_attempts_allowed == Number_of_attempts_allowed) {
+      cout << "You have " << remaining_number_of_attempts_allowed << " chances left. Wish you all the best. Good luck! \n";
+    }
+      else if (remaining_number_of_attempts_allowed > 5) {
+        cout << "You have " << remaining_number_of_attempts_allowed << " chances left. \n";
+    }
+      else if (remaining_number_of_attempts_allowed > 3) {
+        cout << "You still have " << remaining_number_of_attempts_allowed << " chances left. I hope you can guess the correct number within the chances left. \n";
+      }
+      else if (remaining_number_of_attempts_allowed > 1) {
+        cout << "You have only " << remaining_number_of_attempts_allowed << " chances left. Please seize the only chances left. \n";
+      }
+      else {
+        cout << "You have only 1 chance left. Seems like you are rather unlucky now. I hope you can create the miracle of guessing the correct number with such a faint chance. \n";
+      }
+    }
+    
+    if (Number_of_terms_of_vector_v == 4) {
+      cout << "Please make a guess (1 - 16). ";
+    if (remaining_number_of_attempts_allowed == Number_of_attempts_allowed) {
+      cout << "You have " << remaining_number_of_attempts_allowed << " chances left. Wish you all the best. Good luck! \n";
+    }
+    else if (remaining_number_of_attempts_allowed > 7) {
+        cout << "You have " << remaining_number_of_attempts_allowed << " chances left. \n";
+    }
+      else if (remaining_number_of_attempts_allowed > 3) {
+        cout << "You still have " << remaining_number_of_attempts_allowed << " chances left. I hope you can guess the correct number within the chances left. \n";
+      }
+      else if (remaining_number_of_attempts_allowed > 1) {
+        cout << "You have only " << remaining_number_of_attempts_allowed << " chances left. Please seize the only chances left. \n";
+      }
+      else {
+        cout << "You have only 1 chance left. Seems like you are rather unlucky now. I hope you can create the miracle of guessing the correct number with such a faint chance. \n";
+      }
+    }
+    
+    cin >> guess;
+    
+    if (guess == number_to_be_guessed) {
+      is_guessed_correctly = true;
+      numbers_guessed_right.push_back(number_to_be_guessed);
+      if (remaining_number_of_attempts_allowed == Number_of_attempts_allowed) {
+        cout << "WOW! YOU'VE GUESSED THE CORRECT NUMBER WITHOUT NEEDING MORE ATTEMPTS! \nBRAVISSIMO!!! Seems like you are super lucky today!!! I hope your luck will last long!!" << endl;
+        break;
+      }
+      else if (remaining_number_of_attempts_allowed >= Number_of_attmepts_allowed - 3) {
+        cout << "Wow! You have guessed the correct number with only a few attempts! \nBravo! You are very lucky today!! I hope your luck will last long!!" << endl;
+        break;
+      }
+      else if (remaining_number_of_attempts_allowed > 3) {
+        cout << "You've guessed the correct number without needing too many chances. \nSeems like you are quite lucky today. I hope your luck will last long and wish you a luckier future." << endl;
+        break;
+      }
+      else if (remaining_number_of_attempts_allowed > 1) {
+        cout << "You've guessed the correct number finally. You are very near to the failure but fortunately you have successfully seized your chances. \nI wish you a luckier future." << endl;
+        break;
+      }
+      if (remaining_number_of_attempts_allowed == 1) {
+        cout << "WOW! Congratulations that you've really made the miracle! \nI believe that you will be much luckier in the future. " << endl;
+        break;
+      }
+      break;
+      }
+    if (guess != number_to_be_guessed) {
+    remaining_number_of_attempts_allowed--;
+    }
+  }
+  if (is_guessed_correctly = false) {
+    cout << "I am so sorry that you can't make the miracle. You still cannot guess the correct number. \nBut I believe that you will be luckier in the future." << endl;
+  }
+}
+
 int main() {
   cout << "Welcome to this game! \nTry your luck and see if you are lucky or not today! \n\n"; //Welcoming
   
-  vector<int>remaining_numbers_not_guessed;
-  vector<int>numbers_already_guessed;
-  vector<int>numbers_guessed_right;
   // vector of numbers guessed wrong: no need
     // instruction code
     char instruction_code;
@@ -56,12 +149,11 @@ int main() {
     if (instruction_code == 'S') {
       //run new game
       int Number_of_terms_of_vector_v; //Decide either 3*3 or 4*4 grid
-      int Number_of_attempts_allowed; //universal number of attempt allowed
       cout << "Input '3' to have a 3x3 bingo board or '4' to have a 4x4 bingo board" << endl;
       cin >> Number_of_terms_of_vector_v;
       if (Number_of_terms_of_vector_v == 3) {
         int a=0;
-        while (a<Number_of_terms_of_vector_v^2) {
+        while (a<Number_of_terms_of_vector_v * Number_of_terms_of_vector_v) {
           b = a + 1;
           remaining_numbers_not_guessed.push_back(b);
         }
@@ -71,18 +163,20 @@ int main() {
       }
       if (Number_of_terms_of_vector_v == 4) {
         int a=0;
-        while (a<Number_of_terms_of_vector_v^2) {
+        while (a<Number_of_terms_of_vector_v * Number_of_terms_of_vector_v) {
           b = a + 1;
           remaining_numbers_not_guessed.push_back(b);
         }
         remaining_numbers_not_guessed = processed(remaining_numbers_not_guessed, numbers_already_guessed);
-        Number_of_attempts_allowed = 12; //declare max number of attempt if input grid is 4
+        Number_of_attempts_allowed = 10; //declare max number of attempt if input grid is 4
       }
       if (Number_of_terms_of_vector_v != 3 && Number_of_terms_of_vector_v != 4) {
         cout << "Sorry, please input either '3' or '4' only. Thank you. " << endl;
         cin >> Number_of_terms_of_vector_v;
       }
-      run_game(remaining_numbers_not_guessed, Number_of_attempts_allowed); //run the 'guessing random number' game
+      //run the 'guessing random number' game
+      run_guessing_random_number_game(remaining_numbers_not_guessed, Number_of_attempts_allowed, Number_of_terms_of_vector_v); 
+      
       //load bingo board
       
     }

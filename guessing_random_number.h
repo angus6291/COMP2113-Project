84cc6c802ed1a356915fbb8dd75e8bdec7820ed4
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-02 19:41:57 +0800

    Update guessing_random_number.h

--- guessing_random_number.h
+++ guessing_random_number.h
@@ -1,6 +1,8 @@
+//guessing_random_number.h
 #ifndef GUESSING_RANDOM_NUMBER_H
 #define GUESSING_RANDOM_NUMBER_H
 
-void run_guessing_random_number_game(remaining_numbers_not_guessed, Number_of_attempts_allowed, Number_of_terms_of_vector_v);
+void print_bingo_board(vector<string>bingo_board, int Number_of_terms_of_vector_v, vector<int>remaining_numbers_not_guessed, vector<int> numbers_guessed_right, vector<int> numbers_guessed_wrong);
+void run_guessing_random_number_game(vector<int>remaining_numbers_not_guessed, int Number_of_attempts_allowed, int Number_of_terms_of_vector_v);
 
 #endif

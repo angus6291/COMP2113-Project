0452fcfcc5c5155a0297aa905d462d78a0230238
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-03 17:52:49 +0800

    Update guessing_random_number.h

--- guessing_random_number.h
+++ guessing_random_number.h
@@ -1,6 +1,7 @@
 //run.h
-#ifndef RUN_H
-#define RUN_H
+#ifndef GUESSING_RANDOM_NUMBER_H
+#define GUESSING_RANDOM_NUMBER_H
 
+bool run(vector<int> to_be_guessed, int node_conversion);
 void run_guessing_random_number_game(vector<int> to_be_guessed, vector<char> out_put);
 #endif

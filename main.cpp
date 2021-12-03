57811cecccbda429f6e6995bfe9d60d6c1dd4a0e
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-03 16:11:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream> // for file input and output ('saved game')
 #include <cstdlib> // for srand(), rand()
 #include <algorithm>
+#include "guessing_random_number.h"
 #include "run.h"
 using namespace std;
 
@@ -62,78 +63,6 @@ void load(vector<int>& to_be_guessed, vector<char>& out_put) {
     fin.close();
 }
   
-void run_guessing_random_number_game(vector<int>& to_be_guessed, vector<char>& out_put) { // only run once
-     //showing the pre-any in-put bingo board
-      cout << "Your current status: (New Game Version) " << endl;
-      for (int i = 0; i<9; i=i+3) {
-        for (int a = 0; a<3; a++) {
-          cout << out_put[i+a] << "   ";
-        }
-        cout << endl;
-      }
-
-      //how many nodes left?
-      int number_of_node_guessed = 0;
-      for (int i=0; i<9; i++) {
-        if (out_put[i]>='A' && out_put[i]<='J') {
-          number_of_node_guessed++;
-        }
-      }
-      //main body
-      int number_of_node_available = number_of_node_guessed;
-      while (number_of_node_available>0) {
-        char node_input;
-        cout << "Please input letters from A to I for the guessing. Do not pick either 'X' (wrong) or 'O' (correct), as those are your result. " << endl;
-        cin >> node_input;
-        int max_guessing_chance = 3;
-        int node_conversion = int(node_input) - int('A');
-        if (run(to_be_guessed, max_guessing_chance, node_conversion)) {out_put[node_conversion] = 'O';}
-        else {out_put[node_conversion] = 'X';}
-        cout << "Your updated grid: " << endl;
-        for (int i = 0; i<9; i=i+3) {
-          for (int a = 0; a<3; a++) {
-            cout << out_put[i+a] << " ";
-          }
-          cout << endl;
-        }
-        number_of_node_available--;
-      }
-
-      //result checking
-      if (out_put[0] == 'O' && out_put[3] == 'O' && out_put[6] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      else if (out_put[1] == 'O' && out_put[4] == 'O' && out_put[7] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      else if (out_put[2] == 'O' && out_put[5] == 'O' && out_put[8] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      if (out_put[0] == 'O' && out_put[1] == 'O' && out_put[2] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      if (out_put[3] == 'O' && out_put[4] == 'O' && out_put[5] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      if (out_put[6] == 'O' && out_put[7] == 'O' && out_put[8] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      if (out_put[0] == 'O' && out_put[4] == 'O' && out_put[8] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      if (out_put[2] == 'O' && out_put[4] == 'O' && out_put[7] == 'O') {
-        cout << "Congrats u got one or more than one bingo" << endl;
-        break;
-      }
-      }
-
 int main() {
   cout << "Welcome to this game! \nTry your luck and see if you are lucky or not today! \n\n"; //Welcoming
     // instruction code

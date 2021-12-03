0c290a9308b120f63e892c7e6f2d133066e51e68
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-03 16:30:43 +0800

    Update run.h

--- run.h
+++ run.h
@@ -1,5 +1,5 @@
 #ifndef RUN_H
 #define RUN_H
 
-bool run(vector<int>& to_be_guessed, int max_guessing_chance, int node_conversion);
+bool run(vector<int> to_be_guessed, int node_conversion);
 #endif

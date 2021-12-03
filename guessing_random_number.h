2184eef95dff38b67b70ddeb0cf0d5a3b4a8db12
angus6291 <93399469+angus6291@users.noreply.github.com>
2021-12-03 21:31:48 +0800

    change name to run.h and finalised version

--- guessing_random_number.h
+++
@@ -1,8 +0,0 @@
-//run.h
-#ifndef GUESSING_RANDOM_NUMBER_H
-#define GUESSING_RANDOM_NUMBER_H
-
-void run_guessing_random_number_game(vector<int> to_be_guessed, vector<char> out_put);
-bool run(vector<int> to_be_guessed, int node_conversion);
-
-#endif

2b029e17bbe151bd0fc6915aa0e8a8f74a2bd16a
angus6291 <93399469+angus6291@users.noreply.github.com>
2021-12-03 11:45:37 +0800

    run.cpp as one of the function in main
    run.cpp

---
+++ run.cpp
@@ -0,0 +1,27 @@
+//run.cpp for run game!!
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <fstream> // for file input and output ('saved game')
+#include <cstdlib> // for srand(), rand()
+#include <algorithm>
+#include "run.h"
+using namespace std;
+
+bool run(const vector<int>& to_be_guessed, int max_guessing_chance, int node_conversion) {
+  int correct_ans = to_be_guessed[node_coversion];
+  cout << "Your maximum guessing chances: " << max_guessing_chance << ". Good Luck." << endl;
+  for (i=1; max_guessing_chance>=i; i++) {
+    int user_input_for_node;
+    cin >> user_input_for_node;
+    if(user_input_for_node == correct_ans) {
+      cout << "Congrats, for the selected node, u hv the right ans within maximum guessing chance, 'O' will be shown in the board" << endl;
+      return true;
+    if(user_input_for_node != correct_ans && max_guessing_chance != i) {
+      cout << "Please retry guessing." << endl;
+    }
+    }
+  }
+  cout << "Unfortunately, for the selected node, u doesn't hv the right ans within maximum guessing chance, 'X' will be shown in the board" << endl;
+  return false;
+}

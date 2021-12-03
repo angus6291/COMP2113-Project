e635ac07ba46d4c165a030cf747529f2bceb26c9
JOHN TSE <90818320+LuxiferSagerking623@users.noreply.github.com>
2021-12-03 16:30:10 +0800

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -8,20 +8,26 @@
 #include "run.h"
 using namespace std;
 
-bool run(vector<int>& to_be_guessed, int max_guessing_chance, int node_conversion) {
+bool run(vector<int>& to_be_guessed, int node_conversion) { // return true if guessed successfully, false if not guessed successfully
   int correct_ans = to_be_guessed[node_coversion];
-  cout << "Your maximum guessing chances: " << max_guessing_chance << ". Good Luck." << endl;
-  for (i=1; max_guessing_chance>=i; i++) {
+  cout << "You have only 3 chances". Good Luck." << endl;
+  int i=3;
+  while (i>0) {
     int user_input_for_node;
     cin >> user_input_for_node;
     if(user_input_for_node == correct_ans) {
-      cout << "Congrats, for the selected node, u hv the right ans within maximum guessing chance, 'O' will be shown in the board" << endl;
+      cout << "Congratulations! You have guessed the right letter within the given chances!" << endl;
       return true;
-    if(user_input_for_node != correct_ans && max_guessing_chance != i) {
+      break;
+    }
+    if(user_input_for_node != correct_ans && i < 3) {
       cout << "Please retry guessing." << endl;
     }
-    }
+    i--;
   }
-  cout << "Unfortunately, for the selected node, u doesn't hv the right ans within maximum guessing chance, 'X' will be shown in the board" << endl;
+  
+  if (i==0) {
+  cout << "I am sorry that your chances are used up and you still haven't guessed the right letter. " << endl;
   return false;
+}
 }
